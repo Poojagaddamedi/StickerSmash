a4c788dec2add14209f3b6961817298f746ba016
S-Axhwin <ashwin.sathiyamoorthy.d@gmail.com>
2025-01-10 21:22:53 +0530

    add auth backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
-
+import * as dotenv from 'dotenv';
 import userRoute from "./routes/userRoute";
+
+dotenv.config();
 
 const app = new Hono()
 app.use(cors())
--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -2,35 +2,45 @@ import { Hono } from "hono"
 
 const userRoute = new Hono();
 
-const users = [
-    {
-        regNo: "12308018",
-        password: "ashwin@2005"
-    },
-    {
-        regNo: "12308019",
-        password: "pooja@2005"
-    }
-];
+import supabase from "../database/db";
+
 
  
 userRoute.post("/login", async (c) => {
     const { regNo, password } = await c.req.json();
     
-    const user = users.find(user => user.regNo === regNo && user.password === password);
+    const {data, error} = await supabase
+                            .from("user")
+                            .select("*")
+                            .eq("regNo", regNo)
+                            .eq("password", password);
     
-    if (user) {
-        return c.json({ mes: "you have logged in", user });
+    if (data?.length > 0) {
+        return c.json({ mes: "you have logged in", user: data, error });
     } else {
         return c.json({ mes: "invalid credentials" }, 401);
     }
 });
 
 
-userRoute.post("reg", async (c) => {
+userRoute.post("/reg", async (c) => {
     const { regNo, password } = await c.req.json();
-    users.push({regNo, password});
+    const {data: user, error: userError} = await supabase
+                            .from("user")
+                            .select("*")
+                            .eq("regNo", regNo);
+    console.log(user);
+    if (user?.length > 0) {
+        return c.json({ mes: "user already exists", userError }, 401);
+    }
+    const {data: userData, error: userDataError} = await supabase
+                            .from("user")
+                            .insert({regNo, password});
+    if (userDataError) {
+        return c.json({ mes: "user already exists", userDataError }, 401);
+    }
     return c.json({ mes: "user registered" });
 });
+
 
 export default userRoute;
